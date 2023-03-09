3d762125dd3a421406c7d63cd1e1b7f52ef4067e
Liyue-Wei <126851525+Liyue-Wei@users.noreply.github.com>
2023-03-09 09:27:23 +0800

    README.md

--- 學習歷程專案/OpenCV/WebCam_2.py
+++
@@ -1,9 +0,0 @@
-from selenium import webdriver
-import time
-
-driver = webdriver.Chrome()
-driver.get("http://192.168.137.171:8080/shot.jpg")
-
-while True:
-    time.sleep(0.04)
-    driver.refresh()
--- 學習歷程專案/math_tools/一元二次方程式/tk_test.py
+++
@@ -1,9 +0,0 @@
-import tkinter as tk
-win = tk.Tk()
-win.geometry("1280x720")
-win.title("test")
-
-label1 = tk.Label(win, text="test", fg='black')
-label1.pack()
-
-win.mainloop()
