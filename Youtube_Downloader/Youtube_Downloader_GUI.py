89647d2a459f49a507aeefc5cff89b8a0aac9f32
Liyue-Wei <126851525+Liyue-Wei@users.noreply.github.com>
2023-03-22 18:45:32 +0800

    Youtube_Downloader_GUI.py

--- Youtube_Downloader/Youtube_Downloader_GUI.py
+++ Youtube_Downloader/Youtube_Downloader_GUI.py
@@ -8,28 +8,12 @@ win.geometry("1280x720")
 win.title("YouTube Downloader")
 win.resizable(0, 0)
 
-def path():
-    file_path = os.path.abspath(os.path.dirname(__file__))
-    file_path = file_path.replace("\\", "/")
-    length = len(file_path)
-    file_path_2 = []
-    for i in range(0, length):          
-        file_path_2.append(file_path[i])
-
-    return (file_path_2)
-
-def path_function(direct_path):
-    file_path = path()
-    file_path.append(direct_path)
-    file_path = "".join(file_path)
-    return(file_path)
-
 class GUI_interface:
     ttk.Label(win, text="YouTube Downloader", font=("微軟正黑體", 20)).place(x=10, y=10)
     ttk.Label(win, text="輸入影片網址", font=("微軟正黑體", 14)).place(x=10, y=90)
     ttk.Entry(win, font=("微軟正黑體", 16), width=57).place(x=10, y=135)
     tk.Button(win, text="Search", font=("微軟正黑體", 13)).place(x=1116, y=135, width=154, height=184)
-    ttk.Label(win, text="下載位置 (預設為下載)", font=("微軟正黑體", 14)).place(x=10, y=220)
+    ttk.Label(win, text="下載位置 (預設為Download)", font=("微軟正黑體", 14)).place(x=10, y=220)
     ttk.Entry(win, font=("微軟正黑體", 16), width=57).place(x=10, y=265)
     ttk.Label(win, text="影片資訊", font=("微軟正黑體", 14)).place(x=10, y=350)
     ttk.Frame(win, height=5, width=1260, style="darkly").place(x=10, y=395)
