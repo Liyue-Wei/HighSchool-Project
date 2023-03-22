fd75591b4558ccc5d05c425c0da819837b851bb7
Liyue-Wei <126851525+Liyue-Wei@users.noreply.github.com>
2023-03-22 09:46:35 +0800

    Youtube_Downloader_GUI.py

--- Youtube_Downloader/Youtube_Downloader_GUI.py
+++ Youtube_Downloader/Youtube_Downloader_GUI.py
@@ -3,7 +3,7 @@ import tkinter as tk
 import ttkbootstrap as ttk
 import os
 
-win = ttk.Window(themename="cerculean")
+win = ttk.Window(themename="darkly")
 win.geometry("1280x720")
 win.title("YouTube Downloader")
 win.resizable(0, 0)
@@ -12,6 +12,11 @@ class GUI_interface:
     ttk.Label(win, text="YouTube Downloader", font=("微軟正黑體", 20)).place(x=10, y=10)
     ttk.Label(win, text="輸入影片網址", font=("微軟正黑體", 14)).place(x=10, y=90)
     ttk.Entry(win, font=("微軟正黑體", 16), width=57).place(x=10, y=135)
-    tk.Button(win, text="Download", font=("微軟正黑體", 13)).place(x=1122, y=135)
+    tk.Button(win, text="Search", font=("微軟正黑體", 13)).place(x=1116, y=135, width=154, height=184)
+    ttk.Label(win, text="下載位置 (預設為下載)", font=("微軟正黑體", 14)).place(x=10, y=220)
+    ttk.Entry(win, font=("微軟正黑體", 16), width=57).place(x=10, y=265)
+    ttk.Label(win, text="影片資訊", font=("微軟正黑體", 14)).place(x=10, y=350)
+    ttk.Frame(win, height=5, width=1260, style="darkly").place(x=10, y=395)
+    tk.Button(win, text="Download", font=("微軟正黑體", 13)).place(x=1116, y=415, width=154, height=294)
 
     win.mainloop()
