784ef4f842fad06facd60d8e64c49d7733a8c5e4
Liyue-Wei <126851525+Liyue-Wei@users.noreply.github.com>
2023-03-22 18:23:19 +0800

    Youtube_Downloader_GUI.py

--- Youtube_Downloader/Youtube_Downloader_GUI.py
+++ Youtube_Downloader/Youtube_Downloader_GUI.py
@@ -8,6 +8,22 @@ win.geometry("1280x720")
 win.title("YouTube Downloader")
 win.resizable(0, 0)
 
+def path():
+    file_path = os.path.abspath(os.path.dirname(__file__))
+    file_path = file_path.replace("\\", "/")
+    length = len(file_path)
+    file_path_2 = []
+    for i in range(0, length):          
+        file_path_2.append(file_path[i])
+
+    return (file_path_2)
+
+def path_function(direct_path):
+    file_path = path()
+    file_path.append(direct_path)
+    file_path = "".join(file_path)
+    return(file_path)
+
 class GUI_interface:
     ttk.Label(win, text="YouTube Downloader", font=("微軟正黑體", 20)).place(x=10, y=10)
     ttk.Label(win, text="輸入影片網址", font=("微軟正黑體", 14)).place(x=10, y=90)
