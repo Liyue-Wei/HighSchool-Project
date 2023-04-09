2f1bef1a3985f30b2e65bb7935435d77fdc7b58a
Zhe Wei Chang <126851525+Liyue-Wei@users.noreply.github.com>
2023-04-09 14:15:21 +0800

    Update install.py

--- tools/install.py
+++ tools/install.py
@@ -1,5 +1,4 @@
 import os
-
 modules = ["ttkbootstrap", "pyautogui", "selenium", "openpyxl", "opencv-python", "numpy", "markdown", "matplotlib", "pytube", "requests", "pillow", "pygame", "pandas", "sympy", "openai"]
 for i in range(0, len(modules)):
     os.system("pip install "+modules[i])
