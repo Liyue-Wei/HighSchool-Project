aeaf7970ef50c1270ef8f9327b421f00ff4ec073
Zhe Wei Chang <126851525+Liyue-Wei@users.noreply.github.com>
2023-04-17 11:06:39 +0800

    commit

--- Zerojudge/teacher lee's judge/a196.py
+++ Zerojudge/teacher lee's judge/a196.py
@@ -2,7 +2,7 @@ while True:
     s, e, b, k = map(int, input().split(' '))
     i = 0
     for n in range(s, e+1):
-        if(n%10==b or n%b==0 or int((n%100)/10)==b):
+        if(n%10==b or n%b==0 or int((n%100)/10 or int(n%1000)/100)==b):
             i+=1
         if(i==k):
             break
