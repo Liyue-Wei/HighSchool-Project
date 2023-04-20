dd7b96b5ae62c36ce0534a5e495059f341294bf1
Zhe Wei Chang <126851525+Liyue-Wei@users.noreply.github.com>
2023-04-20 20:06:15 +0800

    pytube壞掉了，今天沒辦法寫

--- Youtube_Music_Downloader/pt2_test.py
+++ Youtube_Music_Downloader/pt2_test.py
@@ -31,9 +31,11 @@ def append():
             playlist.append(YT)
             pll_title.append(info)
             status_txt.insert(INSERT, (info+'\n'))
+            url.set("")
         
         except:
             messagebox.showerror("載入失敗", "無法載入網址")
+            url.set("")
 
 def clear():
     status_txt.delete('1.0', 'end')
@@ -62,6 +64,21 @@ def music_download():
             messagebox.showerror("下載失敗", "查無此影片")
 '''
 
+def select():
+    print("select")
+
+def delete():
+    print("del")
+
+def DL():
+    print("DL")
+
+def info():
+    print("info")
+
+def err_list():
+    print("err_list")
+
 class GUI_interface:
     global status_txt
     ttk.Label(win, text="YouTube Music Downloader", font=("微軟正黑體", 20)).place(x=10, y=10)
@@ -69,10 +86,10 @@ class GUI_interface:
     ttk.Entry(win, font=("微軟正黑體", 16), width=67, textvariable=url).place(x=10, y=137)
     tk.Button(win, text="Append", font=("微軟正黑體", 13), command=append).place(x=1116, y=131, width=154, height=62)  
     ttk.Label(win, text="下載位置 (預設為Download)", font=("微軟正黑體", 14)).place(x=10, y=220)
-    tk.Button(win, text="Select", font=("微軟正黑體", 13)).place(x=528, y=261, width=154, height=62)
+    tk.Button(win, text="Select", font=("微軟正黑體", 13), command=select).place(x=528, y=261, width=154, height=62)
     ttk.Frame(win, height=192, width=5, style="darkly").place(x=695, y=204)
     ttk.Label(win, text="編輯預下載列表", font=("微軟正黑體", 14)).place(x=713, y=220)
-    tk.Button(win, text="Delete", font=("微軟正黑體", 13)).place(x=1116, y=261, width=154, height=62)
+    tk.Button(win, text="Delete", font=("微軟正黑體", 13), command=delete).place(x=1116, y=261, width=154, height=62)
     tk.Radiobutton(win, text="由網址刪除", font=("微軟正黑體", 11)).place(x=713, y=340)
     tk.Radiobutton(win, text="由歌名刪除", font=("微軟正黑體", 11)).place(x=884, y=340)
     tk.Radiobutton(win, text="由錯誤列表自動刪除", font=("微軟正黑體", 11)).place(x=1050, y=340)
@@ -90,8 +107,8 @@ class GUI_interface:
     tk.Radiobutton(win, text="webm - 160kbps", font=("微軟正黑體", 13)).place(x=847, y=651)
     status_txt = tk.Text(win, font=("微軟正黑體", 12))
     status_txt.place(x=15, y=413, width=803, height=220)
-    tk.Button(win, text="Download List", font=("微軟正黑體", 13)).place(x=15, y=645, width=168, height=62)
-    tk.Button(win, text="Information", font=("微軟正黑體", 13)).place(x=195, y=645, width=154, height=62)
-    tk.Button(win, text="Error List", font=("微軟正黑體", 13)).place(x=360, y=645, width=154, height=62)
+    tk.Button(win, text="Download List", font=("微軟正黑體", 13), command=DL).place(x=15, y=645, width=168, height=62)
+    tk.Button(win, text="Information", font=("微軟正黑體", 13), command=info).place(x=195, y=645, width=154, height=62)
+    tk.Button(win, text="Error List", font=("微軟正黑體", 13), command=err_list).place(x=360, y=645, width=154, height=62)
     tk.Button(win, text="Clear", font=("微軟正黑體", 13), command=clear).place(x=664, y=645, width=154, height=62)
     win.mainloop()
