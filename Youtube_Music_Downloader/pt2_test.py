7161688c976d253c0e3470cf1bb048f3743eed25
Zhe Wei Chang <126851525+Liyue-Wei@users.noreply.github.com>
2023-04-20 18:24:00 +0800

    Update pt2_test.py

--- Youtube_Music_Downloader/pt2_test.py
+++ Youtube_Music_Downloader/pt2_test.py
@@ -7,7 +7,7 @@ import ttkbootstrap as ttk
 # import os
 
 win = ttk.Window(themename="darkly")
-win.geometry("1280x595")
+win.geometry("1280x720")
 win.title("YouTube Music")
 win.resizable(0, 0)
 
@@ -18,7 +18,7 @@ playlist = []
 pll_title = []
 error_list = []
 
-def search():
+def append():
     vid_url = url.get()
     YT = YouTube(vid_url)
     info = YT.title
@@ -26,7 +26,7 @@ def search():
     pll_title.append(info)
     status_txt.insert(INSERT, (info+'\n'))
 
-def delete():
+def clear():
     status_txt.delete('1.0', 'end')
 
 def video_download():
@@ -58,34 +58,17 @@ class GUI_interface:
     ttk.Label(win, text="YouTube Music Downloader", font=("微軟正黑體", 20)).place(x=10, y=10)
     ttk.Label(win, text="輸入YouTube網址", font=("微軟正黑體", 14)).place(x=10, y=90)
     ttk.Entry(win, font=("微軟正黑體", 16), width=67, textvariable=url).place(x=10, y=137)
-    tk.Button(win, text="Search", font=("微軟正黑體", 13), command=search).place(x=1116, y=131, width=154, height=62)
-    ttk.Label(win, text="詳細資訊", font=("微軟正黑體", 14)).place(x=10, y=225)
-    ttk.Frame(win, height=5, width=1260, style="darkly").place(x=10, y=270)
-    tk.Button(win, text="Download", font=("微軟正黑體", 13), command=video_download).place(x=1116, y=290, width=148, height=288)
-    ttk.Frame(win, height=315, width=5, style="darkly").place(x=1095, y=270)
-    ttk.Frame(win, height=315, width=5, style="darkly").place(x=831, y=270)
-    tk.Label(win, text="Format", font=("微軟正黑體", 13)).place(x=847, y=295)
-    tk.Radiobutton(win, text="mp4 - 48kbps", font=("微軟正黑體", 13)).place(x=847, y=346)
-    tk.Radiobutton(win, text="mp4 - 128kbps", font=("微軟正黑體", 13)).place(x=847, y=408)
-    tk.Radiobutton(win, text="webm - 50kbps", font=("微軟正黑體", 13)).place(x=847, y=466)
-    tk.Radiobutton(win, text="webm - 160kbps", font=("微軟正黑體", 13)).place(x=847, y=526)
-    status_txt = tk.Text(win, font=("微軟正黑體", 12))
-    status_txt.place(x=15, y=288, width=803, height=290)
-    
-    win.mainloop()
-
-'''    
+    tk.Button(win, text="Append", font=("微軟正黑體", 13), command=append).place(x=1116, y=131, width=154, height=62)  
     ttk.Label(win, text="下載位置 (預設為Download)", font=("微軟正黑體", 14)).place(x=10, y=220)
     tk.Button(win, text="Select", font=("微軟正黑體", 13)).place(x=528, y=261, width=154, height=62)
     ttk.Frame(win, height=192, width=5, style="darkly").place(x=695, y=204)
     ttk.Label(win, text="編輯預下載列表", font=("微軟正黑體", 14)).place(x=713, y=220)
-    tk.Button(win, text="Delete", font=("微軟正黑體", 13), command=delete).place(x=1116, y=261, width=154, height=62)
+    tk.Button(win, text="Delete", font=("微軟正黑體", 13)).place(x=1116, y=261, width=154, height=62)
     tk.Radiobutton(win, text="由網址刪除", font=("微軟正黑體", 11)).place(x=713, y=340)
     tk.Radiobutton(win, text="由歌名刪除", font=("微軟正黑體", 11)).place(x=884, y=340)
-    tk.Radiobutton(win, text="由錯誤自動刪除", font=("微軟正黑體", 11)).place(x=1055, y=340)
-    ttk.Entry(win, font=("微軟正黑體", 16), width=30).place(x=10, y=265)
-    ttk.Entry(win, font=("微軟正黑體", 16), width=23).place(x=713, y=265)
-
+    tk.Radiobutton(win, text="由錯誤列表自動刪除", font=("微軟正黑體", 11)).place(x=1050, y=340)
+    ttk.Entry(win, font=("微軟正黑體", 16), width=30).place(x=10, y=268)
+    ttk.Entry(win, font=("微軟正黑體", 16), width=23).place(x=713, y=268)
     ttk.Label(win, text="詳細資訊", font=("微軟正黑體", 14)).place(x=10, y=350)
     ttk.Frame(win, height=5, width=1260, style="darkly").place(x=10, y=395)
     tk.Button(win, text="Download", font=("微軟正黑體", 13), command=video_download).place(x=1116, y=415, width=148, height=288)
@@ -97,5 +80,9 @@ class GUI_interface:
     tk.Radiobutton(win, text="webm - 50kbps", font=("微軟正黑體", 13)).place(x=847, y=591)
     tk.Radiobutton(win, text="webm - 160kbps", font=("微軟正黑體", 13)).place(x=847, y=651)
     status_txt = tk.Text(win, font=("微軟正黑體", 12))
-    status_txt.place(x=15, y=413, width=803, height=290)
-'''
+    status_txt.place(x=15, y=413, width=803, height=220)
+    tk.Button(win, text="Download List", font=("微軟正黑體", 13)).place(x=15, y=645, width=168, height=62)
+    tk.Button(win, text="Information", font=("微軟正黑體", 13)).place(x=195, y=645, width=154, height=62)
+    tk.Button(win, text="Error List", font=("微軟正黑體", 13)).place(x=360, y=645, width=154, height=62)
+    tk.Button(win, text="Clear", font=("微軟正黑體", 13), command=clear).place(x=664, y=645, width=154, height=62)
+    win.mainloop()
