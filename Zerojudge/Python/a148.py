9862fe1bba1adac7e1f98c0a442309efba374de0
Zhe Wei Chang <126851525+Liyue-Wei@users.noreply.github.com>
2023-05-02 20:38:30 +0800

    想不出為何WA

--- Zerojudge/Python/a148.py
+++ Zerojudge/Python/a148.py
@@ -1,10 +1,13 @@
-i = int(input())
-n = []
-t = 0
-for l in range(0, i):
-    t+=int(input())
-if(t/i>=60):
-    print("no")
-else:
-    print("yes")
-    
+i = []
+while(True):
+    try:
+        i = input().split(' ')
+        t = 0
+        for n in range(1, len(i)):
+            t+=int(i[n])
+        if(t/int(i[0])>=60):
+            print("no")
+        else:
+            print("yes")
+    except:
+        break
