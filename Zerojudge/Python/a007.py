e9922d40cec7c09ad7f23a63cd1948e1fa318a1b
Zhe Wei Chang <126851525+Liyue-Wei@users.noreply.github.com>
2023-05-03 11:48:56 +0800

    sympy

---
+++ Zerojudge/Python/a007.py
@@ -0,0 +1,14 @@
+
+
+'''
+from sympy import *
+while(True):
+    try:
+        i = int(input())
+        if(isprime(i)):
+            print("質數")
+        else:
+            print("非質數")
+    except:
+        break
+'''
