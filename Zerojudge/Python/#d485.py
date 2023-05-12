379bd8be0844903e706bc4f6bdcffb18fc8c3635
Zhe Wei Chang <126851525+Liyue-Wei@users.noreply.github.com>
2023-05-12 20:54:11 +0800

    odd number

--- Zerojudge/Python/#d485.py
+++ Zerojudge/Python/#d485.py
@@ -1,10 +1,13 @@
 i, n = map(int, input().split(' '))
-l=0
+l = 0
 if(i%2!=0):
     i+=1
-for t in range(i, n+1, 2):
-    if(t%2==0):
-        l+=1
+if(n%2!=0):
+    n+=1
+h = (n-i)/2
+# for t in range(i, n+1, 2):
+#     if(t%2==0):
+#         l+=1
 print(l)
 
 '''
