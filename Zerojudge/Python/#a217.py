682a31015f6295a4c376df514ae5ecd52fee6b36
Zhe Wei Chang <126851525+Liyue-Wei@users.noreply.github.com>
2023-05-12 20:32:50 +0800

    a217連清育都解決不了

--- Zerojudge/Python/#a217.py
+++ Zerojudge/Python/#a217.py
@@ -1,13 +1,28 @@
 import re
 while(True):
     try:
+        def capitalize_sentence(sentence):
+            sentence = sentence.capitalize()
+            sentence = re.sub(r'\bi\b', 'I', sentence)
+            sentence = re.sub(r'(?<=[.!?])\s+([a-z])', lambda x: x.group(0).upper(), sentence)
+            return sentence
+        input_text = input()
+        output_text = capitalize_sentence(input_text)
+        print(output_text)
+    except:
+        break
+
+'''
+import re
+while(True):
+    try:
         b = str(input())
         t = b
         a = []
         for l in range(len(t)):
             if(t[l]=="." or t[l]=="!" or t[l]=="?"):
                 a.append(t[l])
-        b = re.split('\. |\! |\? |\!|\ i', b)
+        b = re.split('\. |\! |\? |\!', b)
         if(len(b)!=1):
             b.pop()
             for n in range(len(b)):
@@ -21,3 +36,4 @@ while(True):
             print('\n')
     except:
         break
+'''
