2173cfce7ce30e0d966e81b7ac33a6b11463fede
Zhe Wei Chang <126851525+Liyue-Wei@users.noreply.github.com>
2023-05-18 12:00:58 +0800

    Update cupy_speed_trial.py

--- tools/cupy_speed_trial.py
+++ tools/cupy_speed_trial.py
@@ -4,15 +4,15 @@ import os
 import time
 
 t_now = float(time.time())
-for i in range(10000):
-    x = cp.arange(1000000).reshape(1000, 1000).astype('f')
-print(x, x.sum(axis=1))
+for i in range(1000):
+    x = cp.arange(100000000).reshape(10000, 10000).astype('f')
+# print(x)
 print("count with cupy by cuda {} \n".format(float(time.time())-t_now))
 
 t_now = float(time.time())
-for i in range(10000):
-    x = np.arange(1000000).reshape(1000, 1000).astype('f')
-print(x, x.sum(axis=1))
+for i in range(1000):
+    x = np.arange(100000000).reshape(10000, 10000).astype('f')
+# print(x)
 print("count with numpy by cpu {} \n".format(float(time.time())-t_now))
 
 os.system('pause')
