fafa8de8f5edcf3ca10d0b4890513958cb376815
Zhe Wei Chang <126851525+Liyue-Wei@users.noreply.github.com>
2023-05-21 12:58:09 +0800

    Update cupy_speed_trial.py

--- tools/cupy_speed_trial.py
+++ tools/cupy_speed_trial.py
@@ -1,18 +1,25 @@
-import cupy as cp
-import numpy as np
 import os
+import numpy as np
 import time
 
-t_now = float(time.time())
-for i in range(10000):
-    x = cp.arange(1000000).reshape(1000, 1000).astype('f')
-# print(x)
-print("count with cupy by cuda {} \n".format(float(time.time())-t_now))
+def program():
+    t_now = float(time.time())
+    for i in range(10000):
+        x = cp.arange(1000000).reshape(1000, 1000).astype('f')
+    print("count with cupy by cuda {} \n".format(float(time.time())-t_now))
 
-t_now = float(time.time())
-for i in range(10000):
-    x = np.arange(1000000).reshape(1000, 1000).astype('f')
-# print(x)
-print("count with numpy by cpu {} \n".format(float(time.time())-t_now))
+    t_now = float(time.time())
+    for i in range(10000):
+        x = np.arange(1000000).reshape(1000, 1000).astype('f')
+    print("count with numpy by cpu {} \n".format(float(time.time())-t_now))
 
-os.system('pause')
+    os.system('pause')
+
+try:
+    os.system('nvidia-smi.exe')
+    import cupy as cp
+    program()
+
+except:
+    print("Cuda not avaliable")
+    os.system('pause')
