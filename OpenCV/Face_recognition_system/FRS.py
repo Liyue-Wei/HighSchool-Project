05867d806cd9b7fb8fed4bf335f2914bde714444
Zhe Wei Chang <126851525+Liyue-Wei@users.noreply.github.com>
2023-07-13 13:55:56 +0800

    Update FRS.py
    try迴圈有順利執行，但是依然沒辦法改變畫面分辨率

--- OpenCV/Face_recognition_system/FRS.py
+++ OpenCV/Face_recognition_system/FRS.py
@@ -6,24 +6,27 @@ import os
 import glob
 
 cam = cv2.VideoCapture(0)
-cam.set(cv2.CAP_PROP_FRAME_WIDTH, 1280)
-cam.set(cv2.CAP_PROP_FRAME_HEIGHT, 720)
+try:
+    cam.set(cv2.CAP_PROP_FRAME_WIDTH, 1280)
+    cam.set(cv2.CAP_PROP_FRAME_HEIGHT, 720)
+    # print("{}x{} {}fps".format(1280, 720, 30))
 
-width = int(cam.get(cv2.CAP_PROP_FRAME_WIDTH))  
-height = int(cam.get(cv2.CAP_PROP_FRAME_HEIGHT))
-fps = int(cam.get(cv2.CAP_PROP_FPS))
-print("{}x{} {}fps".format(width, height, fps))
+except:
+    width = int(cam.get(cv2.CAP_PROP_FRAME_WIDTH))  
+    height = int(cam.get(cv2.CAP_PROP_FRAME_HEIGHT))
+    fps = int(cam.get(cv2.CAP_PROP_FPS))
+    print("{}x{} {}fps".format(width, height, fps))
 
 if not cam.isOpened():
-    print("Cannot open camera")
+    print("Camera is not available")
     exit()
 
-# while True:
-#     ret, img = cam.read()
-#     if not ret:
-#         print("Camera not available")
-#         break
-# 
-#     cv2.imshow('FRS', img)   
-#     if cv2.waitKey(1) == 27:    #ESC
-#         break             
+while True:
+    ret, img = cam.read()
+    if not ret:
+        print("Camera is not available")
+        break
+
+    cv2.imshow('FRS', img)   
+    if cv2.waitKey(1) == 27:    #ESC
+        break             
