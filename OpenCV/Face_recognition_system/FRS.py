1d73cbad08b91ff97be95f89a6db730869693aa4
Zhe Wei Chang <126851525+Liyue-Wei@users.noreply.github.com>
2023-07-13 13:49:50 +0800

    設定不了解析度

--- OpenCV/Face_recognition_system/FRS.py
+++ OpenCV/Face_recognition_system/FRS.py
@@ -1,22 +1,29 @@
 import cv2
 import numpy as np
-import cupy as cp
+# import cupy as cp
 import time
 import os
 import glob
 
 cam = cv2.VideoCapture(0)
+cam.set(cv2.CAP_PROP_FRAME_WIDTH, 1280)
+cam.set(cv2.CAP_PROP_FRAME_HEIGHT, 720)
+
+width = int(cam.get(cv2.CAP_PROP_FRAME_WIDTH))  
+height = int(cam.get(cv2.CAP_PROP_FRAME_HEIGHT))
+fps = int(cam.get(cv2.CAP_PROP_FPS))
+print("{}x{} {}fps".format(width, height, fps))
 
 if not cam.isOpened():
     print("Cannot open camera")
     exit()
 
-while True:
-    ret, img = cam.read()
-    if not ret:
-        print("Camera not available")
-        break
-
-    cv2.imshow('FRS', img)    
-    if cv2.waitKey(1) == 27:    #ESC
-        break             
+# while True:
+#     ret, img = cam.read()
+#     if not ret:
+#         print("Camera not available")
+#         break
+# 
+#     cv2.imshow('FRS', img)   
+#     if cv2.waitKey(1) == 27:    #ESC
+#         break             
