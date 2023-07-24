a55758526416ef31145241e3ae7ca5402e9bb028
Zhe Wei Chang <126851525+Liyue-Wei@users.noreply.github.com>
2023-07-24 11:17:12 +0800

    Update install.py
    更新cupy-cuda12x安装规则

--- tools/install.py
+++ tools/install.py
@@ -1,4 +1,13 @@
 import os
+
 modules = ["ttkbootstrap", "pyautogui", "selenium", "openpyxl", "opencv-python", "numpy", "markdown", "matplotlib", "pytube", "requests", "pillow", "pygame", "pandas", "opencv-contrib-python"]
 for i in range(0, len(modules)):
     os.system("pip install "+modules[i])
+
+try:
+    os.system('nvidia-smi.exe')
+    os.system("pip install "+"cupy-cuda12x")    #根据Cuda版本变更cupy安装版本
+
+except:
+    print("Cuda not avaliable")
+    os.system('pause')
