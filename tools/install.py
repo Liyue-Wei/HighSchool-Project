50b6609b345631de67a8466e714d262fb3fe9ff7
Zhe Wei Chang <126851525+Liyue-Wei@users.noreply.github.com>
2023-08-07 19:02:16 +0800

    Update install.py

--- tools/install.py
+++ tools/install.py
@@ -6,7 +6,7 @@ for i in range(0, len(modules)):
 
 try:
     os.system('nvidia-smi.exe')
-    os.system("pip install "+"cupy-cuda12x")    #根据Cuda版本变更cupy安装版本
+    os.system("pip install "+"cupy-cuda12x")    #根据Cuda版本變更cupy安装版本，參考https://docs.nvidia.com/cuda/cuda-toolkit-release-notes/index.html
 
 except:
     print("Cuda environment not avaliable")
