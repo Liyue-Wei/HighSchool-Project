5c6aea083a9b9cf886cd82a4668aabaa9cc73a29
Zhe Wei Chang <126851525+Liyue-Wei@users.noreply.github.com>
2023-08-23 08:25:43 +0800

    Update FRS.py

--- OpenCV/Face_recognition_system/FRS.py
+++ OpenCV/Face_recognition_system/FRS.py
@@ -1,16 +1,11 @@
 import numpy as np
 import cv2
 # import requests
-# import time
+import time
 
 cam = cv2.VideoCapture(0)
 # cam.set(cv2.CAP_PROP_FRAME_WIDTH, 320)
 # cam.set(cv2.CAP_PROP_FRAME_HEIGHT, 240)
-
-width = int(cam.get(cv2.CAP_PROP_FRAME_WIDTH))  
-height = int(cam.get(cv2.CAP_PROP_FRAME_HEIGHT))
-fps = int(cam.get(cv2.CAP_PROP_FPS))
-print("{}x{} {}fps".format(width, height, fps))
 
 if not cam.isOpened():
     print("Camera is not available")
@@ -22,6 +17,11 @@ while True:
         print("Camera is not available")
         break
 
+    width = int(cam.get(cv2.CAP_PROP_FRAME_WIDTH))  
+    height = int(cam.get(cv2.CAP_PROP_FRAME_HEIGHT))
+    fps = int(cam.get(cv2.CAP_PROP_FPS))
+    print("{}x{} {}fps".format(width, height, fps))
+
     # img = cv2.resize(img, (0, 0), fx=2.0, fy=2.0)
     cv2.imshow('FRS', img)
     kernel = np.array([[-1,-1,-1], [-1,9,-1], [-1,-1,-1]])
@@ -29,4 +29,8 @@ while True:
     cv2.imshow('FRS enhanced', img)   
     
     if cv2.waitKey(1) == 27:    #ESC
-        break             
+        break   
+
+    elif cv2.waitKey(1) == 13:    #Enter
+        file_name = ("ScreenShot "+time.ctime(time.time())+ ".jpeg")
+        image = cv2.imwrite(file_name, img)  
