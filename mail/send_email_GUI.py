f0397584eba5f20f93f3dba4d96c60860f040d46
Zhe Wei Chang <126851525+Liyue-Wei@users.noreply.github.com>
2023-09-07 02:35:17 +0800

    GUI update

--- mail/send_email_GUI.py
+++ mail/send_email_GUI.py
@@ -1,2 +1,11 @@
 from send_email_API import send
-import tkinter
+import tkinter as tk
+
+win = tk.Tk()
+win.geometry("1280x720")
+win.title("Email sender")
+win.resizable(0, 0)
+
+global master, slave, title, content, smtp, tcp, password
+
+win.mainloop()
