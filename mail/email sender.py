872e572249caf7f589b50e6fc9b4f644a4a9e6ea
Zhe Wei Chang <126851525+Liyue-Wei@users.noreply.github.com>
2023-09-10 01:46:03 +0800

    Update email sender.py

--- mail/email sender.py
+++ mail/email sender.py
@@ -15,6 +15,12 @@ smtp = tk.StringVar()
 tcp = tk.StringVar()
 password = tk.StringVar()
 
+def settings():
+    print("hello world")
+
+def email():
+    print("send")
+
 class GUI_interface():
     tk.Label(win, text="Sender Address", font=("微軟正黑體", 16)).place(x=5, y=5)
     tk.Entry(win, font=("微軟正黑體", 14), width=45, textvariable=master).place(x=5, y=35)
@@ -24,7 +30,7 @@ class GUI_interface():
     tk.Entry(win, font=("微軟正黑體", 14), width=50, textvariable=title).place(x=5, y=180)
     tk.Label(win, text="content", font=("微軟正黑體", 16)).place(x=5, y=210)
     tk.Text(win, font=("微軟正黑體", 12)).place(x=5, y=245, width=590, height=400)
-    tk.Button(win, text="smtp/tcp settings", font=("微軟正黑體", 14)).place(x=5, y=650)
-    tk.Button(win, text="send", font=("微軟正黑體", 14)).place(x=530, y=650)
+    tk.Button(win, text="smtp/tcp settings", font=("微軟正黑體", 14), command=settings).place(x=5, y=650)
+    tk.Button(win, text="send", font=("微軟正黑體", 14), command=email).place(x=530, y=650)
 
 win.mainloop()
