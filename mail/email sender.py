1119c6204f283943152b01a41c00f2f5cefcf32c
Zhe Wei Chang <126851525+Liyue-Wei@users.noreply.github.com>
2023-09-10 04:35:24 +0800

    Update email sender.py

--- mail/email sender.py
+++ mail/email sender.py
@@ -10,7 +10,7 @@ global master, slave, title, content, smtp, tcp, password
 master = tk.StringVar()
 slave = tk.StringVar()
 title = tk.StringVar()
-content = tk.StringVar()
+# content = tk.StringVar()
 smtp = tk.StringVar()
 tcp = tk.StringVar()
 password = tk.StringVar()
@@ -29,7 +29,7 @@ class GUI_interface():
     tk.Label(win, text="title", font=("微軟正黑體", 16)).place(x=5, y=150)
     tk.Entry(win, font=("微軟正黑體", 14), width=50, textvariable=title).place(x=5, y=180)
     tk.Label(win, text="content", font=("微軟正黑體", 16)).place(x=5, y=210)
-    tk.Text(win, font=("微軟正黑體", 12)).place(x=5, y=245, width=590, height=400)
+    content = tk.Text(win, font=("微軟正黑體", 12)).place(x=5, y=245, width=590, height=400)
     tk.Button(win, text="smtp/tcp settings", font=("微軟正黑體", 14), command=settings).place(x=5, y=650)
     tk.Button(win, text="send", font=("微軟正黑體", 14), command=email).place(x=530, y=650)
 
