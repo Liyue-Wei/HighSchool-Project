4530a35f63e0c1a7a4aaac67d13699b4fff1e678
Zhe Wei Chang <126851525+Liyue-Wei@users.noreply.github.com>
2023-10-24 15:03:41 +0800

    Update b964.py

--- Zerojudge/Python/b964.py
+++ Zerojudge/Python/b964.py
@@ -1,7 +1,24 @@
 i = int(input())
 n = list(map(int, input().split(' ')))
 n.sort()
+T = []
+F = []
+(Max, Min) = (max(n), min(n))
+(worst, best) = (0, 0)
 
-maxiuma, minuma = max(n), min(n)
+for t in range(0, i):
+    if(n[t]>=60):
+        T.append(n[t])
 
-print(n, maxiuma, minuma, end='')
+    else:
+        F.append(n[t])
+
+for t in range(0, i):
+    print(n[t], end=' ')
+
+if(len(T)!=0 and len(F)!=0):
+    print('\n{}\n{}'.format(F[-1], T[0]))
+    
+else:
+    print('\n{}\n{}'.format(F[-1], "worst case")) if(len(T)==0) else print('\n{}\n{}'.format("best case", T[0]))
+    
