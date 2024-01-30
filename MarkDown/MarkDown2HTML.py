6d0b7ca03b40e12915c11c404fd4529d5d73f347
Zhe Wei Chang <126851525+Liyue-Wei@users.noreply.github.com>
2024-01-30 13:11:05 +0800

    內容整理

--- MarkDown/MarkDown2HTML.py
+++ MarkDown/MarkDown2HTML.py
@@ -1,9 +1,49 @@
-import tkinter as tk
-from tkinter import messagebox
+# import tkinter as tk
+# from tkinter import messagebox
 import markdown
 import codecs
 import os
 
+global file_name
+def path():
+    file_path = os.path.abspath(os.path.dirname(__file__))
+    length = len(file_path)
+    file_path_2 = []
+    for i in range(0, length):       
+        if file_path[i] == "\\":
+            file_path_2.append("/")
+
+        else :
+            file_path_2.append(file_path[i])
+
+    return (file_path_2)
+
+def path_function():
+    file_path = path()
+    direct_path = ("/"+str(file_name.get()))
+    file_path.append(direct_path)
+    file_path = "".join(file_path)
+    return(file_path)
+
+def convert():
+    try:
+        file_input = codecs.open((str(path_function())+".md"), mode="r", encoding="utf-8")
+        md_text = file_input.read()
+        html_text = markdown.markdown(md_text)
+        file_output = codecs.open((str(file_name.get())+".html"), mode="w", encoding="utf-8")
+        file_output.write(html_text)
+        
+        print("完成")
+        # messagebox.showinfo(title="Compelete", message="完成")
+
+    except:
+        print("查無此文件")
+        # messagebox.showerror(title="Error", message="查無此文件")
+
+file_name = input()
+convert()
+
+'''
 win = tk.Tk()
 win.geometry("700x148")
 win.title("MarkDown 2 HTML")
@@ -50,3 +90,4 @@ tk.Entry(win, text="file name", font=("微軟正黑體", 16), width=52, textvari
 tk.Button(win, text="Enter", font=("微軟正黑體", 12), command=convert).pack(side=tk.LEFT, anchor=tk.N, padx=5, pady=0)
 
 win.mainloop()
+'''
