f1181a270cc4fa3be52c94b7df7f941d6c8a5be9
Zhe Wei Chang <126851525+Liyue-Wei@users.noreply.github.com>
2024-07-04 04:56:17 +0800

    卷積核算法更新

--- OpenCV/Face_recognition_system/FRS.py
+++ OpenCV/Face_recognition_system/FRS.py
@@ -20,25 +20,29 @@ height = int(cam.get(cv2.CAP_PROP_FRAME_HEIGHT))
 fps = int(cam.get(cv2.CAP_PROP_FPS))
 print("{}x{} {}fps".format(width, height, fps))
 
+upscale = 8.0
+downscale = 1/8
+
 while True:
     ret, img = cam.read()
     if not ret:
         print("Camera is not available")
         break
 
-    # img = cv2.resize(img, (0, 0), fx=2.0, fy=2.0)
     cv2.imshow('FRS', img)
     
+    img = cv2.resize(img, (0, 0), fx=upscale, fy=upscale)
     kernel = np.array([[-1,-1,-1], [-1,9,-1], [-1,-1,-1]])  
     img = cv2.filter2D(img, -1, kernel)
+    img = cv2.resize(img, (0, 0), fx=downscale, fy=downscale)
     cv2.imshow('FRS enhanced', img) 
-    key = cv2.waitKey(1)
-    
+
+    key = cv2.waitKey(1)    
     if key == 27:    #ESC
         break   
 
-    elif key == 13:    #Enter
-        file_name = ("ScreenShot "+time.ctime(time.time())+ ".jpeg")
-        image = cv2.imwrite(file_name, img)  
+    # elif key == 13:    #Enter
+    #     file_name = ("ScreenShot "+time.ctime(time.time())+ ".jpeg")
+    #     image = cv2.imwrite(file_name, img)  
 
 cv2.destroyAllWindows()
